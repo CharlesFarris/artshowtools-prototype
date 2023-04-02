44f614b3aa52c62546b7e5af0f252f734e2aeea5
Charles Farris <charlesfarris71@gmail.com>
2023-04-02 16:53:01 -0600

    Add event handler
    - Add tests

--- src/Domain/Artwork.fs
+++ src/Domain/Artwork.fs
@@ -49,6 +49,41 @@ and ArtworkInfo =
       Year: ArtworkYear }
 
 module Artwork =
+    let private applyEvent result event : Result<Artwork, ArtworkError> =
+        match result with
+        | Error _ -> result
+        | Ok artwork ->
+            match event with
+            | Created created ->
+                match artwork with
+                | Initial ->
+                    Ok(
+                        Existing
+                            { Id = created.Id
+                              Title = created.Title
+                              Year = created.Year }
+                    )
+                | _ -> Error ArtworkAlreadyExists
+            | TitleChanged titleChanged ->
+                match artwork with
+                | Initial -> Error ArtworkDoesNotExist
+                | Existing existing ->
+                    if (existing.Id = titleChanged.Id) then
+                        Ok(Existing { existing with Title = titleChanged.Title })
+                    else
+                        Error WrongArtwork
+            | YearChanged yearChanged ->
+                match artwork with
+                | Initial -> Error ArtworkDoesNotExist
+                | Existing existing ->
+                    if (existing.Id = yearChanged.Id) then
+                        Ok(Existing { existing with Year = yearChanged.Year })
+                    else
+                        Error WrongArtwork
+
+    let apply artwork events : Result<Artwork, ArtworkError> =
+        events |> List.fold applyEvent (Ok artwork)
+
     let handle artwork command : Result<ArtworkEvent list, ArtworkError> =
         match command with
         | Create create ->
@@ -62,4 +97,34 @@ module Artwork =
                     |> List.singleton
                 )
             | Existing _ -> Error ArtworkAlreadyExists
-        | _ -> Ok List.empty
+        | ChangeTitle changeTitle ->
+            match artwork with
+            | Initial _ -> Error ArtworkDoesNotExist
+            | Existing existing ->
+                if (existing.Id = changeTitle.Id) then
+                    Ok(
+                        TitleChanged
+                            { Id = changeTitle.Id
+                              Title = changeTitle.Title }
+                        |> List.singleton
+                    )
+                else
+                    Error WrongArtwork
+        | ChangeYear changeYear ->
+            match artwork with
+            | Initial -> Error ArtworkDoesNotExist
+            | Existing existing ->
+                if (existing.Id = changeYear.Id) then
+                    Ok(
+                        YearChanged
+                            { Id = changeYear.Id
+                              Year = changeYear.Year }
+                        |> List.singleton
+                    )
+                else
+                    Error WrongArtwork
+
+    let applyAndHandle events command : Result<ArtworkEvent list, ArtworkError> =
+        events
+        |> apply Initial
+        |> Result.bind (fun artwork -> command |> handle artwork)
--- tests/Domain.Tests/ArtworkTests.fs
+++ tests/Domain.Tests/ArtworkTests.fs
@@ -7,25 +7,142 @@ let id = Guid("350EA2A6-6316-44DE-9316-2D545E5CA2C5") |> ArtworkId
 let title = "Title" |> ArtworkTitle
 let year = 2023 |> ArtworkYear
 let artwork = Existing { Id = id; Title = title; Year = year }
+let wrongId = Guid("D96CD8A5-E757-4798-8753-769E710AFA5A") |> ArtworkId
+
+let wrongArtwork =
+    Existing
+        { Id = wrongId
+          Title = title
+          Year = year }
+
+let newTitle = "New Title" |> ArtworkTitle
+let newYear = 2025 |> ArtworkYear
+
+[<Test>]
+let ``Artwork.apply Created succeeds`` () =
+    let events = Created { Id = id; Title = title; Year = year } |> List.singleton
+    let result = events |> Artwork.apply Initial
+
+    match result with
+    | Ok updated ->
+        match updated with
+        | Initial _ -> Assert.Fail("unexpected state")
+        | Existing existing ->
+            Assert.That(existing.Id, Is.EqualTo(id))
+            Assert.That(existing.Title, Is.EqualTo(title))
+            Assert.That(existing.Year, Is.EqualTo(year))
+    | Error _ -> Assert.Fail("apply should succeed")
+
+[<Test>]
+let ``Artwork.apply Created fails if artwork exists`` () =
+    let events = Created { Id = id; Title = title; Year = year } |> List.singleton
+    let result = events |> Artwork.apply artwork
+
+    match result with
+    | Ok _ -> Assert.Fail("apply should not succeed")
+    | Error error ->
+        match error with
+        | ArtworkAlreadyExists -> ()
+        | _ -> Assert.Fail("unexpected error type")
+
+[<Test>]
+let ``Artwork.apply TitleChanged succeeds`` () =
+    let events = TitleChanged { Id = id; Title = newTitle } |> List.singleton
+    let result = events |> Artwork.apply artwork
+
+    match result with
+    | Ok updated ->
+        match updated with
+        | Initial _ -> Assert.Fail("unexpected state")
+        | Existing existing ->
+            Assert.That(existing.Id, Is.EqualTo(id))
+            Assert.That(existing.Title, Is.EqualTo(newTitle))
+    | Error _ -> Assert.Fail("apply should have succeeded")
+
+[<Test>]
+let ``Artwork.apply TitleChanged fails if artwork does not exist`` () =
+    let events = TitleChanged { Id = id; Title = newTitle } |> List.singleton
+    let result = events |> Artwork.apply Initial
+
+    match result with
+    | Ok _ -> Assert.Fail("apply should not have succeeded")
+    | Error error ->
+        match error with
+        | ArtworkDoesNotExist -> ()
+        | _ -> Assert.Fail("unexpected error type")
+
+[<Test>]
+let ``Artwork.apply TitleChanged fails on wrong ID`` () =
+    let events = TitleChanged { Id = id; Title = newTitle } |> List.singleton
+    let result = events |> Artwork.apply wrongArtwork
+
+    match result with
+    | Ok _ -> Assert.Fail("apply should not have succeeded")
+    | Error error ->
+        match error with
+        | WrongArtwork -> ()
+        | _ -> Assert.Fail("unexpected error type")
+
+
+[<Test>]
+let ``Artwork.apply YearChanged succeeds`` () =
+    let events = YearChanged { Id = id; Year = newYear } |> List.singleton
+    let result = events |> Artwork.apply artwork
+
+    match result with
+    | Ok updated ->
+        match updated with
+        | Initial -> Assert.Fail("unexpected state")
+        | Existing existing ->
+            Assert.That(existing.Id, Is.EqualTo(id))
+            Assert.That(existing.Year, Is.EqualTo(newYear))
+    | Error _ -> Assert.Fail("apply should have succeeded")
+
+[<Test>]
+let ``Artwork.apply YearChanged fails if artwork does not exist`` () =
+    let events = YearChanged { Id = id; Year = newYear } |> List.singleton
+    let result = events |> Artwork.apply Initial
+
+    match result with
+    | Ok _ -> Assert.Fail("apply should not have succeeded")
+    | Error error ->
+        match error with
+        | ArtworkDoesNotExist -> ()
+        | _ -> Assert.Fail("unexpected error type")
+
+[<Test>]
+let ``Artwork.apply YearChanged fails on wrong ID`` () =
+    let events = YearChanged { Id = id; Year = newYear } |> List.singleton
+    let result = events |> Artwork.apply wrongArtwork
+
+    match result with
+    | Ok _ -> Assert.Fail("apply should not have succeeded")
+    | Error error ->
+        match error with
+        | WrongArtwork -> ()
+        | _ -> Assert.Fail("unexpected error type")
 
 [<Test>]
 let ``Artwork.handle Create fails if artwork exists`` () =
     let command = Create { Id = id; Title = title; Year = year }
     let result = command |> Artwork.handle artwork
+
     match result with
     | Ok _ -> Assert.Fail("command should fail")
     | Error error ->
         match error with
         | ArtworkAlreadyExists -> ()
-        | _ -> Assert.Fail("wrong error type")
+        | _ -> Assert.Fail("unexpected error type")
 
 [<Test>]
 let ``Artwork.handle Create succeeds`` () =
     let command = Create { Id = id; Title = title; Year = year }
     let result = command |> Artwork.handle Initial
+
     match result with
     | Ok events ->
         Assert.That(events.Length, Is.EqualTo(1))
+
         match events |> List.head with
         | Created created ->
             Assert.That(created.Id, Is.EqualTo(id))
@@ -33,3 +150,103 @@ let ``Artwork.handle Create succeeds`` () =
             Assert.That(created.Year, Is.EqualTo(year))
         | _ -> Assert.Fail("unexpected event type")
     | Error _ -> Assert.Fail("command should succeed")
+
+[<Test>]
+let ``Artwork.handle ChangeTitle fails if artwork does not exist`` () =
+    let command = ChangeTitle { Id = id; Title = newTitle }
+    let result = command |> Artwork.handle Initial
+
+    match result with
+    | Ok _ -> Assert.Fail("command should not succeed")
+    | Error error ->
+        match error with
+        | ArtworkDoesNotExist -> ()
+        | _ -> Assert.Fail("unexpected error type")
+
+[<Test>]
+let ``Artwork.handle ChangeTitle fails on wrong ID`` () =
+    let command = ChangeTitle { Id = id; Title = newTitle }
+    let result = command |> Artwork.handle wrongArtwork
+
+    match result with
+    | Ok _ -> Assert.Fail("command should not succeed")
+    | Error error ->
+        match error with
+        | WrongArtwork -> ()
+        | _ -> Assert.Fail("unexpected error type")
+
+[<Test>]
+let ``Artwork.handle ChangeTitle succeeds`` () =
+    let command = ChangeTitle { Id = id; Title = newTitle }
+    let result = command |> Artwork.handle artwork
+
+    match result with
+    | Ok events ->
+        Assert.That(events.Length, Is.EqualTo(1))
+
+        match events |> List.head with
+        | TitleChanged titleChanged ->
+            Assert.That(titleChanged.Id, Is.EqualTo(id))
+            Assert.That(titleChanged.Title, Is.EqualTo(newTitle))
+        | _ -> Assert.Fail("unexpected event type")
+    | Error _ -> Assert.Fail("command should succeed")
+
+[<Test>]
+let ``Artwork.handle ChangeYear fails if artwork does not exist`` () =
+    let command = ChangeYear { Id = id; Year = newYear }
+    let result = command |> Artwork.handle Initial
+
+    match result with
+    | Ok _ -> Assert.Fail("command should not succeed")
+    | Error error ->
+        match error with
+        | ArtworkDoesNotExist -> ()
+        | _ -> Assert.Fail("unexpected error type")
+
+[<Test>]
+let ``Artwork.handle ChangeYear fails on wrong ID`` () =
+    let command = ChangeYear { Id = id; Year = newYear }
+    let result = command |> Artwork.handle wrongArtwork
+
+    match result with
+    | Ok _ -> Assert.Fail("command should not succeed")
+    | Error error ->
+        match error with
+        | WrongArtwork -> ()
+        | _ -> Assert.Fail("unexpected error type")
+
+[<Test>]
+let ``Artwork.handle ChangeYear succeeds`` () =
+    let command = ChangeYear { Id = id; Year = newYear }
+    let result = command |> Artwork.handle artwork
+
+    match result with
+    | Ok events ->
+        Assert.That(events.Length, Is.EqualTo(1))
+
+        match events |> List.head with
+        | YearChanged yearChanged ->
+            Assert.That(yearChanged.Id, Is.EqualTo(id))
+            Assert.That(yearChanged.Year, Is.EqualTo(newYear))
+        | _ -> Assert.Fail("unexpected event type")
+    | Error _ -> Assert.Fail("command should not succeed")
+
+[<Test>]
+let ``testing binding`` () =
+    let events = [
+        Created { Id = id; Title = title; Year = year }
+        TitleChanged { Id = id; Title = newTitle }
+        YearChanged { Id = id; Year = newYear }
+    ]
+    let finalTitle = "Final Title" |> ArtworkTitle
+    let command = ChangeTitle { Id = id; Title = finalTitle }
+    match events |> Artwork.apply Initial with
+    | Ok artwork ->
+        match command |> Artwork.handle artwork with
+        | Ok events ->
+            match updated with
+            | Existing existing ->
+                Assert.That(existing.Id, Is.EqualTo(id))
+                Assert.That(existing.Ti)
+        | Error _ -> Assert.Fail("command should succeed")
+    | Error _ -> Assert.Fail("apply should succeed")
